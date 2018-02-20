9e44e80f46a3bd319aee4ea9b5b820beecc8273f
Mendoza <menpedro@amazon.com>
2018-02-20 17:15:15 +0100

    upgrade to v4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Version number="3"/>
+        <Version number="4"/>
       </div>
     );
   }

aa1115a873e5faad4b457aa31b137e3b2d41fb5b
Mendoza <menpedro@amazon.com>
2018-02-20 12:18:19 +0100

    upgrade to v3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This is version 2.
+          This is version 3.
         </p>
       </div>
     );

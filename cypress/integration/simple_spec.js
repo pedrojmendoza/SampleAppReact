2728dbf51a5b6d4e2f5d01c126ad20d4473b94aa
Mendoza <menpedro@amazon.com>
2018-03-06 11:37:06 +0100

    fix cypress test

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', function() {
   it('Visits the React app', function() {
     cy.visit('http://menpedro-react-app.s3-website-us-east-1.amazonaws.com/')
-    cy.contains('version 4')
+    cy.contains('Welcome to React')
   })
 })

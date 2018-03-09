03c254ed8774a7bd62ce162050a99f1d16160150
Mendoza <menpedro@amazon.com>
2018-03-09 20:11:42 +0100

    adding bang

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
         <FeatureToggle show={process.env.REACT_APP_ES_FEATURE === "true"}>
           <SpainWizard/>

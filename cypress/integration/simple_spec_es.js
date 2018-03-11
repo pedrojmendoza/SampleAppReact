585c9020a309e27e213bfcc3d81b78a9f55dab61
Mendoza <menpedro@amazon.com>
2018-03-11 16:45:18 +0100

    adding country specific tests

--- cypress/integration/simple_spec_es.js
+++ cypress/integration/simple_spec_es.js
@@ -2,5 +2,10 @@ describe('My First Test', function() {
   it('Visits the React app', function() {
     cy.visit('http://menpedro-react-app-preprod-es.s3-website-us-east-1.amazonaws.com/')
     cy.contains('Welcome to React')
+    cy.contains('This is the welcome step')
+    cy.contains('Next').click()
+    cy.contains('This is a Spain compliance step')
+    cy.contains('Next').click()
+    cy.contains('This is yet another (common) step')
   })
 })
--- cypress/integration/simple_spec_us.js
+++ cypress/integration/simple_spec_us.js
@@ -2,5 +2,10 @@ describe('My First Test', function() {
   it('Visits the React app', function() {
     cy.visit('http://menpedro-react-app-preprod-us.s3-website-us-east-1.amazonaws.com/')
     cy.contains('Welcome to React')
+    cy.contains('This is the welcome step')
+    cy.contains('Next').click()
+    cy.contains('This is yet another (common) step')
+    cy.contains('Next').click()
+    cy.contains('This is a US compliance step')
   })
 })

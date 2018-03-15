50f2133010c32f615d55d2be3df3a85a16ddab3f
Mendoza <menpedro@amazon.com>
2018-03-15 11:54:48 +0100

    adding credit card number to force security test to fail

--- src/SpainComplianceStep.jsx
+++ src/SpainComplianceStep.jsx
@@ -4,6 +4,7 @@ class SpainComplianceStep extends Component {
   render() {
     return (
       <p>This is a Spain compliance step.</p>
+      <p>This is a credit card number 4111111111111111</p>
     );
   }
 }

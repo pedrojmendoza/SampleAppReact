f6b8e34d05f8311eceb48ecbdb925952766d731c
Mendoza <menpedro@amazon.com>
2018-03-15 12:03:47 +0100

    fixing linting prob (line len) with credit card number

--- src/SpainComplianceStep.jsx
+++ src/SpainComplianceStep.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SpainComplianceStep extends Component {
   render() {
     return (
-      <p>This is a Spain compliance step. With credit card number (for security vulnerability purposes) 4111111111111111</p>
+      <p>This is a Spain compliance step. 4111111111111111</p>
     );
   }
 }
